4d6296ef51e2813e6d76ba7bbfe84180f12575d2
justmiroslav <mostashchuk@kse.org.uaa>
2024-03-12 12:01:30 +0200

    proper command format

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
 import requests, os
 from fetch_url import country_language_dict
 api_key = os.environ.get("IP_API_KEY")
-command_message = 'Enter command (1: - help; 2 - lang; 3 - city; 4 - exit): '
-help_message = ('Command 2(lang) - Enter the country name to get the language;\n'
-                'Command 3(city) - Enter the ip address to get the city;\n'
-                'Command 4(exit) - Exit the program.')
+command_message = 'Enter message (help; LanguageOf <countryName>; IpToCity <IP>; exit): '
+help_message = ('Command LanguageOf - Enter the country name to get the language;\n'
+                'Command IpToCity - Enter the ip address to get the city;\n'
+                'Command exit - Exit the program.')
 
-def process_lang():
-    country_name = input('Enter the country name: ').strip()
-    country = country_name.title().replace(" ", "")
+def process_lang(country_name):
     try:
-        languages = country_language_dict[country]
+        languages = country_language_dict[country_name]
         language = ", ".join(lang for lang in languages)
         print(language)
     except KeyError:
         print("Country not found.")
 
-def process_city():
-    ip_address = input('Enter the ip address: ').strip().replace(" ", "")
+def process_city(ip_address):
     try:
         # url = f"https://api.2ip.ua/geo.json?ip={ip_address}"
         url = f"https://api.ip2location.io/?key={api_key}&ip={ip_address}"
@@ -29,14 +26,17 @@ def process_city():
 
 def main():
     while True:
-        command = input(command_message).strip()
-        if command == '1':
+        message = input(command_message).strip()
+        command_value = message.split(' ', 1)
+        command = command_value[0].lower()
+        value = command_value[1].title().replace(" ", "") if len(command_value) > 1 else None
+        if command == 'help':
             print(help_message)
-        elif command == '2':
-            process_lang()
-        elif command == '3':
-            process_city()
-        elif command == '4':
+        elif command == 'languageof':
+            process_lang(value)
+        elif command == 'iptocity':
+            process_city(value)
+        elif command == 'exit':
             print('Exiting the program...')
             break
         else:
